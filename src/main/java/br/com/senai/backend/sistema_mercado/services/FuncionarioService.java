2075cb61806f8bb12bdcb45be7db544a24a9e0f4
DaviFlag <davimiguezprog@gmail.com>
2025-11-18 14:39:45 -0300

    Add files via upload

---
+++ src/main/java/br/com/senai/backend/sistema_mercado/services/FuncionarioService.java
@@ -0,0 +1,26 @@
+package br.com.senai.backend.sistema_mercado.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.senai.backend.sistema_mercado.models.Funcionario;
+import br.com.senai.backend.sistema_mercado.repositories.FuncionarioRepositorie;
+
+
+
+@Service
+public class FuncionarioService {
+    
+    @Autowired
+    private FuncionarioRepositorie funcionarioRepository;
+
+    
+    public Funcionario cadastrar(Funcionario funcionario) {
+        return funcionarioRepository.save(funcionario);
+    }
+
+        public Funcionario recuperarPorId(Integer id) {
+            return funcionarioRepository.findById(id).get();
+    }
+
+}
